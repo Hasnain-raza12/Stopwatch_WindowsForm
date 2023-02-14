f46381700df85352ec430ccedfd7abb13e323a43
Husnain Raza <Husnain Raza@DESKTOP-604O976>
2023-02-14 04:15:41 -0800

    Add project files.

---
+++ Stop_Watch12/Form1.Designer.cs
@@ -0,0 +1,158 @@
+﻿
+namespace Stop_Watch12
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.Start = new System.Windows.Forms.Button();
+            this.lblHour = new System.Windows.Forms.Label();
+            this.lblMinute = new System.Windows.Forms.Label();
+            this.lblSeconds = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.Stop = new System.Windows.Forms.Button();
+            this.Reset = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // Start
+            // 
+            this.Start.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Start.Location = new System.Drawing.Point(67, 53);
+            this.Start.Name = "Start";
+            this.Start.Size = new System.Drawing.Size(206, 116);
+            this.Start.TabIndex = 0;
+            this.Start.Text = "Start";
+            this.Start.UseVisualStyleBackColor = false;
+            this.Start.Click += new System.EventHandler(this.Start_Click);
+            // 
+            // lblHour
+            // 
+            this.lblHour.AutoSize = true;
+            this.lblHour.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblHour.Location = new System.Drawing.Point(88, 251);
+            this.lblHour.Name = "lblHour";
+            this.lblHour.Size = new System.Drawing.Size(151, 108);
+            this.lblHour.TabIndex = 1;
+            this.lblHour.Text = "00";
+            // 
+            // lblMinute
+            // 
+            this.lblMinute.AutoSize = true;
+            this.lblMinute.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblMinute.Location = new System.Drawing.Point(290, 240);
+            this.lblMinute.Name = "lblMinute";
+            this.lblMinute.Size = new System.Drawing.Size(151, 108);
+            this.lblMinute.TabIndex = 2;
+            this.lblMinute.Text = "00";
+            // 
+            // lblSeconds
+            // 
+            this.lblSeconds.AutoSize = true;
+            this.lblSeconds.Font = new System.Drawing.Font("Microsoft Sans Serif", 48F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSeconds.Location = new System.Drawing.Point(501, 240);
+            this.lblSeconds.Name = "lblSeconds";
+            this.lblSeconds.Size = new System.Drawing.Size(151, 108);
+            this.lblSeconds.TabIndex = 3;
+            this.lblSeconds.Text = "00";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(245, 260);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(44, 64);
+            this.label1.TabIndex = 4;
+            this.label1.Text = ":";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 28F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(462, 260);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 64);
+            this.label2.TabIndex = 5;
+            this.label2.Text = ":";
+            // 
+            // Stop
+            // 
+            this.Stop.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Stop.Location = new System.Drawing.Point(308, 53);
+            this.Stop.Name = "Stop";
+            this.Stop.Size = new System.Drawing.Size(206, 116);
+            this.Stop.TabIndex = 6;
+            this.Stop.Text = "Stop";
+            this.Stop.UseVisualStyleBackColor = false;
+            this.Stop.Click += new System.EventHandler(this.Stop_Click);
+            // 
+            // Reset
+            // 
+            this.Reset.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.Reset.Location = new System.Drawing.Point(548, 53);
+            this.Reset.Name = "Reset";
+            this.Reset.Size = new System.Drawing.Size(206, 116);
+            this.Reset.TabIndex = 7;
+            this.Reset.Text = "Rest";
+            this.Reset.UseVisualStyleBackColor = false;
+            this.Reset.Click += new System.EventHandler(this.Reset_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.Reset);
+            this.Controls.Add(this.Stop);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblSeconds);
+            this.Controls.Add(this.lblMinute);
+            this.Controls.Add(this.lblHour);
+            this.Controls.Add(this.Start);
+            this.Name = "Form1";
+            this.Text = "STOPWATCH";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button Start;
+        private System.Windows.Forms.Label lblHour;
+        private System.Windows.Forms.Label lblMinute;
+        private System.Windows.Forms.Label lblSeconds;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button Stop;
+        private System.Windows.Forms.Button Reset;
+    }
+}
+
---
+++ Stop_Watch12/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Stop_Watch12
+{
+    public partial class Form1 : Form
+    {
+        // declare some attributes
+        int Hour;
+        int Minutes;
+        int Seconds;
+      //  bool stop = true;
+        private Thread stopwatchThread;
+        public Form1()
+        {
+            InitializeComponent();
+            //Initializing attribute
+            Hour = 0;
+            Minutes = 0;
+            Seconds = 0;
+            Stop.Enabled = false;
+            Reset.Enabled = false;
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Start_Click(object sender, EventArgs e)
+        {
+
+            Reset.Enabled = true;
+            Stop.Enabled = true;
+            stopwatchThread = new Thread(DoStart);
+            stopwatchThread.IsBackground = true; // BackGround Thread
+            stopwatchThread.Start(); //stopwatch Starting Thread
+            Start.Enabled = false;
+
+        }
+
+       // Append zero so it gonna look better
+        private string appendZero(double str)
+        {
+            try   
+            {
+                if (str <= 9)
+                {
+                    return "0" + str;
+                }
+                else
+                {
+                    return str.ToString();
+                }
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+        }
+
+        // Define Delegates
+        private delegate void DisplaySecondDelegate(int i);
+        private delegate void DisplayMinutesDelegate(int minutes);
+        private delegate void DisplayHousrDelegate(int i);
+        private delegate void EnableButtonDelegate();
+
+        //
+        private void EnableButton()
+        {
+            Start.Enabled = true;
+        }   
+        private void DisplaySecond(int i)
+        {
+            lblSeconds.Text = appendZero(i);
+        }
+        private void DisplayMinute(int Minutes)
+        {
+            lblMinute.Text = appendZero(Minutes);
+        }
+        private void DisplayHour(int i)
+        {
+            lblHour.Text = appendZero(i);
+        }
+
+        //DoStart Method
+        private void DoStart()
+        {
+            
+            try
+            {
+                for (int i= Seconds; i >=0; i++)
+                {
+                    lblSeconds.Invoke(new DisplaySecondDelegate(DisplaySecond), i);
+                    lblMinute.Invoke(new DisplayMinutesDelegate(DisplayMinute), Minutes);
+                    lblHour.Invoke(new DisplayHousrDelegate(DisplayHour),Hour);
+
+                    Thread.Sleep(1000);
+                    
+                    if (i > 59)
+                    {
+
+                        Minutes++;
+                        i = Seconds;
+                        if (Minutes > 59)
+                        {
+                            Hour++;
+                            Minutes=0;
+                        }
+                    }
+                   
+                }
+                
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+        }
+
+        private void Reset_Click(object sender, EventArgs e)
+        {
+            //Invoking doReset Method
+            doReset();
+
+        }
+
+        private void doReset()
+        {
+            try
+            {
+                //Enable remaining start Button
+                Start.Invoke(new EnableButtonDelegate(EnableButton));
+                Reset.Enabled = false;
+                Stop.Enabled = false;
+
+                // Reset to zero
+                Minutes = 0;
+                Seconds = 0;
+                Hour = 0;
+
+                //for UI
+                lblHour.Text = 0.ToString();
+                lblMinute.Text = 0.ToString();
+                lblSeconds.Text = 0.ToString();
+                stopwatchThread.Abort();
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+            
+        }
+       
+
+        private void Stop_Click(object sender, EventArgs e)
+        {
+
+            // Invoking doStop() Method
+            doStop();
+          
+           
+        }
+
+        private void doStop()
+        {
+
+            try
+            {
+                if (Stop.Text == "Stop")
+                {
+                    Stop.Text = "Resume";
+
+                 
+                    stopwatchThread.Suspend();
+                    Reset.Enabled = false;
+                }
+                else
+                {
+                    Stop.Text = "Stop";
+                    stopwatchThread.Resume();
+                    Reset.Enabled = true;
+
+                }
+            }
+            catch (Exception e)
+            {
+
+                throw e;
+            }
+
+            
+        }
+
+    }
+}
